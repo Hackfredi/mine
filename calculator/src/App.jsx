e97f033f83d094d317b0412d4ff5eea040de37e4
Devang Bisht <fredigaming4302@gmail.com>
2025-07-12 13:17:01 +0530

    Update App.jsx

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext";
 import MainLayout from "./components/layout/MainLayout";
 import Home from "./pages/Home/Home";
 import Pricing from "./pages/Pricing/Pricing";
@@ -9,34 +10,43 @@ import Testimonials from "./pages/Testimonials/Testimonials";
 import Support from "./pages/Support/Support";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import AccountSettings from "./pages/Account/AccountSettings";
 import NotFound from "./pages/NotFound/NotFound";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="account" element={<AccountSettings />} />
-        {/* Main layout wraps all pages */}
-        <Route path="/" element={<MainLayout />}>
-          {/* Public routes */}
-          <Route index element={<Home />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="how-it-works" element={<HowItWorks />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="testimonials" element={<Testimonials />} />
-          <Route path="support" element={<Support />} />
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          {/* Main layout wraps all public pages */}
+          <Route path="/" element={<MainLayout />}>
+            {/* Public routes */}
+            <Route index element={<Home />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="how-it-works" element={<HowItWorks />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="testimonials" element={<Testimonials />} />
+            <Route path="support" element={<Support />} />
 
-          {/* Auth routes */}
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
+            {/* Auth routes (public) */}
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
 
-          {/* 404 handler */}
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </Router>
+            {/* Protected routes (require authentication) */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="account" element={<AccountSettings />} />
+            </Route>
+
+            {/* 404 handler */}
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
