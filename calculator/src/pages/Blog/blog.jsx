af27df265f09fad113984c63e6ee9fb0234cfe13
Devang Bisht <fredigaming4302@gmail.com>
2025-07-12 13:19:50 +0530

    Create blog.jsx

---
+++ calculator/src/pages/Blog/blog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div className="blog-page">
+      <h1>Blog Page</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default Blog;
